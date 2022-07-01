9d8a53ad9600f473e0d3589ba7149aa855720e1f
AtalelB <atalelbaye99@gmail.com>
2022-07-01 14:33:50 -0700

    The last digit

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main -> assign a random number to the variable n each time
+ * it is executed an prints out
+ * based a condition
+ * Return : always 0
+ */
+int main(void)
+{
+int n;
+
+strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	printf(" %d is positive\n", n);
+	if (n == 0)
+	printf(" %d is zero\n", n);
+
+	if (n < 0)
+	{
+	printf(" %d is negative\n", n);
+	}
+return (n);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+ALX low level programming task-2
