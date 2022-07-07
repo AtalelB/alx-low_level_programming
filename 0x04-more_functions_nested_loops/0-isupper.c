0c22420e9227f5f9b7e16607427716c28e25ce35
AtalelB <atalelbaye99@gmail.com>
2022-07-07 11:31:01 -0700

    Afunction that checks for uppercase character.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,17 @@
 
 /**
  * _isupper - function that checks for uppercase character
+ *
  *  @c: return char type
+ *
  *  Return: 1 for fail and 0 for success
  */
+
 int_isupper(int c)
 {
 	int i = isupper(c);
-	if(i > 0)
+
+	if (i > 0)
 	{
 		return (1);
 	}
